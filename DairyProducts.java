9abb5f366b7afa5a9c51b288bacd4ccbb5b99f57
Aryan <aryanshandilya2001@gmail.com>
2022-05-28 09:17:43 +0530

    First Commit

---
+++ DairyProducts.java
@@ -0,0 +1,23 @@
+package supermarket.products;
+
+public class DairyProducts extends GroceryProduct {
+	private Fat fat;
+	
+	public DairyProducts (String name, double price, double discount, Fat fat) {
+		super(name, price, discount);
+		this.fat = fat;
+	}
+	
+	public String display() {
+		return (super.display() + "\nFat Level: "+fat);
+	}
+
+	public Fat getFat() {
+		return fat;
+	}
+
+	public void setFat(Fat fat) {
+		this.fat = fat;
+	}
+
+}
